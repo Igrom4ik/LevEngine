691ae797cb83250a0d68c4098b1f53b6734f834e
LevEngine CI <devops@local>
2025-11-15 16:56:02 +0200

    chore: reset repo to minimal LevEngine template (source + thirdparty placeholders)

---
+++ build_ci/CPackConfig.cmake
@@ -0,0 +1,71 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+set(CPACK_BUILD_SOURCE_DIRS "C:/DEVOPS/LevEngine;C:/DEVOPS/LevEngine/build_ci")
+set(CPACK_CMAKE_GENERATOR "Visual Studio 17 2022")
+set(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+set(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_FILE "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericDescription.txt")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "LevEngine built using CMake")
+set(CPACK_GENERATOR "ZIP")
+set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY "OFF")
+set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY "OFF")
+set(CPACK_INNOSETUP_ARCHITECTURE "x64")
+set(CPACK_INSTALLED_DIRECTORIES "C:/Program Files (x86)/LevEngine/bin;bin;C:/Program Files (x86)/LevEngine/lib;lib;C:/Program Files (x86)/LevEngine/README.md;.")
+set(CPACK_INSTALL_CMAKE_PROJECTS "C:/DEVOPS/LevEngine/build_ci;LevEngine;ALL;/")
+set(CPACK_INSTALL_PREFIX "C:/Program Files (x86)/LevEngine")
+set(CPACK_MODULE_PATH "")
+set(CPACK_NSIS_DISPLAY_NAME "LevEngine 9.10.11")
+set(CPACK_NSIS_INSTALLER_ICON_CODE "")
+set(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+set(CPACK_NSIS_PACKAGE_NAME "LevEngine 9.10.11")
+set(CPACK_NSIS_UNINSTALL_NAME "Uninstall")
+set(CPACK_OUTPUT_CONFIG_FILE "C:/DEVOPS/LevEngine/build_ci/CPackConfig.cmake")
+set(CPACK_OUTPUT_FILE_PREFIX "C:/DEVOPS/LevEngine/build_ci/package")
+set(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericDescription.txt")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "LevEngine release package")
+set(CPACK_PACKAGE_FILE_NAME "LevEngine-9.10.11-Windows")
+set(CPACK_PACKAGE_FILE_NAME "LevEngine-9.10.11-Windows")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "LevEngine 9.10.11")
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "LevEngine 9.10.11")
+set(CPACK_PACKAGE_NAME "LevEngine")
+set(CPACK_PACKAGE_RELOCATABLE "true")
+set(CPACK_PACKAGE_VENDOR "LevEngine Team")
+set(CPACK_PACKAGE_VERSION "9.10.11")
+set(CPACK_PACKAGE_VERSION_MAJOR "9")
+set(CPACK_PACKAGE_VERSION_MINOR "10")
+set(CPACK_PACKAGE_VERSION_PATCH "11")
+set(CPACK_PRODUCTBUILD_DOMAINS "ON")
+set(CPACK_RESOURCE_FILE_LICENSE "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericLicense.txt")
+set(CPACK_RESOURCE_FILE_README "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericDescription.txt")
+set(CPACK_RESOURCE_FILE_WELCOME "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericWelcome.txt")
+set(CPACK_SET_DESTDIR "OFF")
+set(CPACK_SOURCE_7Z "OFF")
+set(CPACK_SOURCE_GENERATOR "")
+set(CPACK_SOURCE_IGNORE_FILES "/\\.git/;/\\.gitignore$;/\\.gitmodules$;/\\.gitattributes$;/\\.vs/;/\\.vscode/;/\\.idea/;.*\\.cpp$;.*\\.c$;.*\\.h$;.*\\.hpp$;~$")
+set(CPACK_SOURCE_OUTPUT_CONFIG_FILE "C:/DEVOPS/LevEngine/build_ci/CPackSourceConfig.cmake")
+set(CPACK_SOURCE_ZIP "OFF")
+set(CPACK_SYSTEM_NAME "win64")
+set(CPACK_THREADS "1")
+set(CPACK_TOPLEVEL_TAG "win64")
+set(CPACK_WIX_INSTALL_SCOPE "perMachine")
+set(CPACK_WIX_SIZEOF_VOID_P "8")
+
+if(NOT CPACK_PROPERTIES_FILE)
+  set(CPACK_PROPERTIES_FILE "C:/DEVOPS/LevEngine/build_ci/CPackProperties.cmake")
+endif()
+
+if(EXISTS ${CPACK_PROPERTIES_FILE})
+  include(${CPACK_PROPERTIES_FILE})
+endif()
---
+++ cmake-test/CPackConfig.cmake
@@ -0,0 +1,70 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+set(CPACK_BUILD_SOURCE_DIRS "C:/DEVOPS/LevEngine;C:/DEVOPS/LevEngine/cmake-test")
+set(CPACK_CMAKE_GENERATOR "Visual Studio 17 2022")
+set(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+set(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_FILE "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericDescription.txt")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "LevEngine built using CMake")
+set(CPACK_GENERATOR "ZIP;TGZ")
+set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY "OFF")
+set(CPACK_INNOSETUP_ARCHITECTURE "x64")
+set(CPACK_INSTALLED_DIRECTORIES "C:/DEVOPS/LevEngine/cmake-test/install/bin;bin;C:/DEVOPS/LevEngine/cmake-test/install/lib;lib;C:/DEVOPS/LevEngine/cmake-test/install/README.md;.")
+set(CPACK_INSTALL_CMAKE_PROJECTS "C:/DEVOPS/LevEngine/cmake-test;LevEngine;ALL;Release")
+set(CPACK_INSTALL_PREFIX "C:/DEVOPS/LevEngine/cmake-test/install")
+set(CPACK_MODULE_PATH "")
+set(CPACK_NSIS_DISPLAY_NAME "LevEngine 1.0.0")
+set(CPACK_NSIS_INSTALLER_ICON_CODE "")
+set(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+set(CPACK_NSIS_PACKAGE_NAME "LevEngine 1.0.0")
+set(CPACK_NSIS_UNINSTALL_NAME "Uninstall")
+set(CPACK_OUTPUT_CONFIG_FILE "C:/DEVOPS/LevEngine/cmake-test/CPackConfig.cmake")
+set(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericDescription.txt")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "LevEngine release package")
+set(CPACK_PACKAGE_FILE_NAME "LevEngine-1.0.0-Windows")
+set(CPACK_PACKAGE_FILE_NAME "LevEngine-1.0.0-Windows")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "LevEngine 1.0.0")
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "LevEngine 1.0.0")
+set(CPACK_PACKAGE_NAME "LevEngine")
+set(CPACK_PACKAGE_RELOCATABLE "true")
+set(CPACK_PACKAGE_VENDOR "LevEngine Team")
+set(CPACK_PACKAGE_VERSION "1.0.0")
+set(CPACK_PACKAGE_VERSION_MAJOR "1")
+set(CPACK_PACKAGE_VERSION_MINOR "0")
+set(CPACK_PACKAGE_VERSION_PATCH "0")
+set(CPACK_PRODUCTBUILD_DOMAINS "ON")
+set(CPACK_RESOURCE_FILE_LICENSE "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericLicense.txt")
+set(CPACK_RESOURCE_FILE_README "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericDescription.txt")
+set(CPACK_RESOURCE_FILE_WELCOME "C:/Program Files/CMake/share/cmake-4.1/Templates/CPack.GenericWelcome.txt")
+set(CPACK_SET_DESTDIR "ON")
+set(CPACK_SOURCE_7Z "ON")
+set(CPACK_SOURCE_GENERATOR "7Z;ZIP")
+set(CPACK_SOURCE_IGNORE_FILES "/[.]git/;/[.]gitignore$;/[.]gitmodules$;/[.]gitattributes$;/[.]vs/;/[.]vscode/;/[.]idea/;.*[.]cpp$;.*[.]c$;.*[.]h$;.*[.]hpp$;~$")
+set(CPACK_SOURCE_IGNORE_FILES "/[.]git/;/[.]gitignore$;/[.]gitmodules$;/[.]gitattributes$;/[.]vs/;/[.]vscode/;/[.]idea/;.*[.]cpp$;.*[.]c$;.*[.]h$;.*[.]hpp$;~$")
+set(CPACK_SOURCE_OUTPUT_CONFIG_FILE "C:/DEVOPS/LevEngine/cmake-test/CPackSourceConfig.cmake")
+set(CPACK_SOURCE_ZIP "ON")
+set(CPACK_SYSTEM_NAME "win64")
+set(CPACK_THREADS "1")
+set(CPACK_TOPLEVEL_TAG "win64")
+set(CPACK_WIX_INSTALL_SCOPE "perMachine")
+set(CPACK_WIX_SIZEOF_VOID_P "8")
+
+if(NOT CPACK_PROPERTIES_FILE)
+  set(CPACK_PROPERTIES_FILE "C:/DEVOPS/LevEngine/cmake-test/CPackProperties.cmake")
+endif()
+
+if(EXISTS ${CPACK_PROPERTIES_FILE})
+  include(${CPACK_PROPERTIES_FILE})
+endif()
