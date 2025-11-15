9c46d9b863709d3c6554c3434d69f4182b814f2d
LevEngine CI <devops@local>
2025-11-15 17:28:11 +0200

    chore(gitignore): update patterns for PVS-Studio and CMake FetchContent deps

--- gen_proj_msvc_ninja.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Build succeeded
-cmake --build --preset %PRESET% || (echo Build failed & exit /b 1)
-cmake --preset %PRESET% || (echo Configure failed & exit /b 1)
-echo Using CMake preset %PRESET%
-if "%PRESET%"=="" set PRESET=otladka
-set PRESET=%1
-:: Usage: gen_proj_msvc_ninja.bat [otladka|reliz]
-
